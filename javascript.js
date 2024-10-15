efdd1814f95c012eff78b5c6389a88f01c5b056b
SaltyChocolat <rexrares009@gmail.com>
2024-10-15 22:53:56 +0300

    Finished functions and gamelogic

--- javascript.js
+++ javascript.js
@@ -1,30 +1,98 @@
+let playerScore = 0;
+let computerScore = 0;
 
-let humanScore=0;
-let computerScore=0;
+let ComputerChoice;
+let PlayerChoice;
 
-function getComputerChoice(){
+function getComputerChoice() {
     let nr;
     nr = Math.random();
-    nr = Math.floor(nr*3)
-    switch(nr){
-        case(0):
+    nr = Math.floor(nr * 3);
+    switch (nr) {
+        case (0):
             return "Rock";
-        case(1):
+        case (1):
             return "Paper";
-        case(2):
-            return "Scissorss";
+        case (2):
+            return "Scissors";
     }
 }
-function getPlayerChoice(){
-    let str = prompt("Rock Papper or Scissors");
-    if (str != ""){
-        str = str.charAt(0).toUpperCase() + str.slice(1).toLocaleLowerCase();
+
+function getPlayerChoice() {
+    let str = prompt("Rock, Paper, or Scissors?");
+    if (str != "") {
+        str = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
         return str;
     }
 }
 
-let ComputerChoice = getComputerChoice();
-let PlayerChoice = getPlayerChoice();
-function playRound(ComputerChoice,PlayerChoice){
-    
+function playRound(ComputerChoice, PlayerChoice) {
+    switch (PlayerChoice) {
+        case "Rock":
+            switch (ComputerChoice) {
+                case "Paper":
+                    alert("You lose! Paper beats Rock");
+                    computerScore += 1;
+                    break;
+                case "Scissors":
+                    alert("You win! Rock beats Scissors");
+                    playerScore += 1;
+                    break;
+                case "Rock":
+                    alert("Tie");
+                    break;
+                default:
+                    alert("Invalid choice for Computer");
+            }
+            break;
+        case "Paper":
+            switch (ComputerChoice) {
+                case "Paper":
+                    alert("Tie");
+                    break;
+                case "Scissors":
+                    alert("You lose! Scissors beats Paper");
+                    computerScore += 1;
+                    break;
+                case "Rock":
+                    alert("You win! Paper beats Rock");
+                    playerScore += 1;
+                    break;
+                default:
+                    alert("Invalid choice for Computer");
+            }
+            break;
+        case "Scissors":
+            switch (ComputerChoice) {
+                case "Paper":
+                    alert("You win! Scissors beats Paper");
+                    playerScore += 1;
+                    break;
+                case "Scissors":
+                    alert("Tie");
+                    break;
+                case "Rock":
+                    alert("You lose! Rock beats Scissors");
+                    computerScore += 1;
+                    break;
+                default:
+                    alert("Invalid choice for Computer");
+            }
+            break;
+        default:
+            alert("Invalid choice for Player");
+    }
+    alert("Player:"+playerScore+"\nComputer:"+computerScore);
 }
+
+function playGame(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        ComputerChoice = getComputerChoice();
+        PlayerChoice = getPlayerChoice();
+        playRound(ComputerChoice, PlayerChoice);
+    }
+    // Display the final scores after all rounds
+    alert("Final Score:\nPlayer: " + playerScore + "\nComputer: " + computerScore);
+}
+
+playGame(5);
